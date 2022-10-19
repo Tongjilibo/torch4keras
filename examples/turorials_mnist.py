ca4a35c15bff5875ffef3144b417d79c09b7fedc
LB <tongjilibo@163.com>
2022-10-19 21:27:54 +0800

    first commit

---
+++ examples/turorials_mnist.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+from torch4keras import BaseModel, Callback, seed_everything
+from torch.utils.data import TensorDataset, DataLoader
+from tqdm import tqdm
+
+seed_everything(42)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# 读取数据
+mnist = torchvision.datasets.MNIST(root='./', download=True)
+x, y = mnist.train_data.unsqueeze(1), mnist.train_labels
+x, y = x.to(device), y.to(device)
+x = x.float() / 255.0    # scale the pixels to [0, 1]
+x_train, y_train = x[:40000], y[:40000]
+train_dataloader = DataLoader(TensorDataset(x_train, y_train), batch_size=8)
+x_test, y_test = x[40000:], y[40000:]
+test_dataloader = DataLoader(TensorDataset(x_test, y_test), batch_size=8)
+
+class Model(BaseModel):
+    def __init__(self):
+        super().__init__()
+        self.model = torch.nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3), nn.ReLU(),
+            nn.MaxPool2d(2, 2), 
+            nn.Conv2d(32, 64, kernel_size=3), nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(7744, 10)
+        )
+    def forward(self, inputs):
+        return self.model(inputs)
+model = Model().to(device)
+model.compile(optimizer=optim.Adam(model.parameters()), loss=nn.CrossEntropyLoss(), metrics=['acc'])
+
+class Evaluator(Callback):
+    """评估与保存
+    """
+    def __init__(self):
+        self.best_acc = 0.
+
+    def on_epoch_end(self, global_step, epoch, logs=None):
+        acc = self.evaluate(test_dataloader)
+
+        if acc > self.best_acc:
+            self.best_test_acc = acc
+            # model.save_weights('best_model.pt')
+        print(f'test_acc: {acc:.5f}, best_test_acc: {self.best_test_acc:.5f}\n')
+
+    # 定义评价函数
+    def evaluate(self, data):
+        total, hit = 1e-5, 0
+        for X, y in tqdm(data, desc='Evaluate'):
+            pred_y = model(X).argmax(dim=-1)
+            hit += pred_y.eq(y).sum().item()
+            total += y.shape[0]
+        return hit/total
+
+if __name__ == '__main__':
+    model.fit(train_dataloader, steps_per_epoch=None, epochs=5, callbacks=Evaluator())
