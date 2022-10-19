33c63f516cbcb9693a3ff7ec76b96e6f30552de8
LB <tongjilibo@163.com>
2022-10-19 23:42:13 +0800

    after_train_step

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='torch4keras',
-    version='0.0.0',
+    version='0.0.1',
     description='use torch like keras',
     long_description='torch4keras: https://github.com/Tongjilibo/torch4keras',
     license='Apache License',
