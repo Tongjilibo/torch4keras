c524707825238d16345e82223112032fe13de2fa
LB <tongjilibo@163.com>
2022-10-21 21:18:23 +0800

    modify readme

--- examples/turorials_mnist.py
+++ examples/turorials_mnist.py
@@ -21,29 +21,29 @@ x_test, y_test = x[40000:], y[40000:]
 test_dataloader = DataLoader(TensorDataset(x_test, y_test), batch_size=8)
 
 # 方式1
-# class MyModel(Model):
-#     def __init__(self):
-#         super().__init__()
-#         self.model = torch.nn.Sequential(
-#             nn.Conv2d(1, 32, kernel_size=3), nn.ReLU(),
-#             nn.MaxPool2d(2, 2), 
-#             nn.Conv2d(32, 64, kernel_size=3), nn.ReLU(),
-#             nn.Flatten(),
-#             nn.Linear(7744, 10)
-#         )
-#     def forward(self, inputs):
-#         return self.model(inputs)
-# model = MyModel().to(device)
-
-# 方式2
-net = torch.nn.Sequential(
+class MyModel(BaseModel):
+    def __init__(self):
+        super().__init__()
+        self.model = torch.nn.Sequential(
             nn.Conv2d(1, 32, kernel_size=3), nn.ReLU(),
             nn.MaxPool2d(2, 2), 
             nn.Conv2d(32, 64, kernel_size=3), nn.ReLU(),
             nn.Flatten(),
             nn.Linear(7744, 10)
         )
-model = BaseModel(net).to(device)
+    def forward(self, inputs):
+        return self.model(inputs)
+model = MyModel().to(device)
+
+# 方式2
+# net = torch.nn.Sequential(
+#             nn.Conv2d(1, 32, kernel_size=3), nn.ReLU(),
+#             nn.MaxPool2d(2, 2), 
+#             nn.Conv2d(32, 64, kernel_size=3), nn.ReLU(),
+#             nn.Flatten(),
+#             nn.Linear(7744, 10)
+#         )
+# model = BaseModel(net).to(device)
 
 model.compile(optimizer=optim.Adam(model.parameters()), loss=nn.CrossEntropyLoss(), metrics=['acc'])
 
