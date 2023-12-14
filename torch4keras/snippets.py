dbbde28ffa140472c609d1357445dc479624f031
LB <tongjilibo@163.com>
2023-12-14 23:00:10 +0800

    add SetLog

--- torch4keras/snippets.py
+++ torch4keras/snippets.py
@@ -11,12 +11,15 @@ import random
 import traceback
 import copy
 import functools
+import logging
+import datetime
 
 try:
     from sklearn.metrics import roc_auc_score
 except ImportError:
     roc_auc_score = None
-        
+
+
 def take_along_dim(input_tensor, indices, dim=None):
     '''兼容部分低版本pytorch没有torch.take_along_dim
     '''
@@ -592,3 +595,39 @@ def set_precision(num, dense_round=1):
         if abs(num) >= prec:
             return round(num, len_ + dense_round)
     return num
+
+
+class SetLog(object):
+    '''同时在文件中和命令行中log文件'''
+    def __init__(self, file_name=None):
+        self.date = str(datetime.date.today()).replace('-','')
+        self.log_path = './Log/'+self.date
+        os.makedirs(self.log_path, exist_ok=True)
+        if file_name is None:
+            file_name = 'Info.log'
+        self.file_name = file_name
+        self.file = os.path.join(self.log_path, file_name)
+        self.init()
+
+    def init(self):
+        # 创建日志器
+        self.log = logging.getLogger(self.file_name)
+        # 日志器默认级别
+        self.log.setLevel(logging.DEBUG)
+        # 日志格式器
+        self.formatter = logging.Formatter("%(asctime)s| %(message)s")
+        self.setHandle()
+        return self.log
+        
+    def setHandle(self):
+        self.log.handlers.clear()
+        # 终端流输出
+        stream_handle = logging.StreamHandler()
+        self.log.addHandler(stream_handle)
+        stream_handle.setLevel(logging.DEBUG)
+        stream_handle.setFormatter(self.formatter)
+        # 文件流输出
+        file_handle = logging.FileHandler(filename=self.file, mode='a', encoding='utf-8')
+        self.log.addHandler(file_handle)
+        file_handle.setLevel(logging.INFO)
+        file_handle.setFormatter(self.formatter)
