d23e989dcd6b17db4bf007d9994ac9a8ca0e527d
Tongjilibo <tongjilibo@163.com>
2024-04-18 11:35:42 -0400

    v0.2.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name='torch4keras',
-    version='v0.2.1.post2',
+    version='v0.2.2',
     description='Use torch like keras',
     long_description=long_description,
     long_description_content_type="text/markdown",
